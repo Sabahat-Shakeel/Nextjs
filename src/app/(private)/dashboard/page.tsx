6bf566dcd745c91fc3eb8800bfacad30e1080f60
syeda <sbtshakeel61@gmail.com>
2024-09-22 05:40:24 +0500

    Routes groups

---
+++ src/app/(private)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+function Dashboard(){
+    return (
+        <div>
+            <h1 className="text-4xl font-extrabold text-cyan-400 underline hover:my-5 hover:duration-700 cursor-pointer">DASHBOARD PAGE</h1>
+        </div>
+    )
+}
+export default Dashboard;
---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,6 @@
+function Login (){
+    return (
+        <div className="text-6xl absolute text-cyan-400 hover:bg-cyan-500  px-2 py-1 rounded hover:text-white font-bold font-mono cursor-pointer">Login page </div>
+    )
+}
+export default Login
